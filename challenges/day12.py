1f9968e9b11096dbe9c68579294b3a798c00d40c
Colin <colinschepers@hotmail.com>
2021-12-15 12:48:48 +0100

    Optimise challenge 12

--- challenges/day12.py
+++ challenges/day12.py
@@ -4,17 +4,19 @@ from typing import Set
 from utils import get_input
 
 
-def count_paths(current: str, visited: Set[str], may_visit_twice: bool) -> int:
+def count_paths(current: str, small_visited: Set[str], may_visit_twice: bool) -> int:
     cnt = 0
     for dest in connections[current]:
         if dest == 'start':
             continue
         elif dest == 'end':
             cnt += 1
-        elif dest[0].isupper() or dest not in visited:
-            cnt += count_paths(dest, visited | {dest}, may_visit_twice)
+        elif dest in big_caves:
+            cnt += count_paths(dest, small_visited, may_visit_twice)
+        elif dest not in small_visited:
+            cnt += count_paths(dest, small_visited | {dest}, may_visit_twice)
         elif may_visit_twice:
-            cnt += count_paths(dest, visited | {dest}, False)
+            cnt += count_paths(dest, small_visited, False)
     return cnt
 
 
@@ -25,6 +27,7 @@ for line in data:
     c1, c2 = line.split("-")
     connections[c1].append(c2)
     connections[c2].append(c1)
+big_caves = {x for x in connections if x[0].isupper()}
 
 print(count_paths('start', set(), False))
 print(count_paths('start', set(), True))
